8ca87c42348d44e4e0d89686fd1b17b898c6f431
prinsobed <prinsobed@gmail.com>
2016-05-03 14:11:48 +0100

    Setup Test Commit

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1314863
+ * Date: 03/05/2016
+ * Time: 14:07
+ */
+
+echo "Good to Go Esq. Kraine";
+echo "Azure (Web and DB) and Github are Working..!";
+
+?>
